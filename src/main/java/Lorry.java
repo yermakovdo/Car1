bae11c56d6161aa9adccb9b1c3a650cea8dfd80c
LeDfee <yermakovdo@gmail.com>
2023-10-09 21:40:02 +0200

    Update Lorry.java
    second commit

--- src/main/java/Lorry.java
+++ src/main/java/Lorry.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Lorry {
+public class Lorry extends Car{
+    private double cCap;
+
+    public Lorry(String make, String carClass, double weight, Driver driver, Engine engine, double cCap) {
+        super(make, carClass, weight, driver, engine);
+        this.cCap = cCap;
+    }
+
+    public double getCCap() {
+        return cCap;
+    }
+
+    public void setCCap(double cCap) {
+        this.cCap = cCap;
+    }
 }
