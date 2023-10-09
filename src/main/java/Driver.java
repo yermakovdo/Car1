d0dde7213811138082ebca7dd38658fda6b7aea3
LeDfee <yermakovdo@gmail.com>
2023-10-09 21:39:04 +0200

    Update Driver.java
    second commit

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Driver {
+class Person {
+    private String fullName;
+
+    public Person(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
+
+public class Driver extends Person {
+    private int drvExp;
+
+    public Driver(String fullName, int drvExp) {
+        super(fullName);
+        this.drvExp = drvExp;
+    }
+
+    public int getDrvExp() {
+        return drvExp;
+    }
+
+    public void setDrvExp(int drvExp) {
+        this.drvExp = drvExp;
+    }
 }
