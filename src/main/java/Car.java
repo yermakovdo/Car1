11d4c829996bf054c097954c3f53426edc7663eb
LeDfee <yermakovdo@gmail.com>
2023-10-09 21:38:41 +0200

    Update Car.java
    second commit

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    private String make;
+    private String carClass;
+    private double weight;
+    private Driver driver;
+    private Engine engine;
+
+    public Car(String make, String carClass, double weight, Driver driver, Engine engine) {
+        this.make = make;
+        this.carClass = carClass;
+        this.weight = weight;
+        this.driver = driver;
+        this.engine = engine;
+    }
+
+    public void start() {
+        System.out.println("Поехали");
+    }
+
+    public void stop() {
+        System.out.println("Останавливаемся");
+    }
+
+    public void turnRight() {
+        System.out.println("Поворот направо");
+    }
+
+    public void turnLeft() {
+        System.out.println("Поворот налево");
+    }
 }
