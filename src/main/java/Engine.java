cee6b9c45f9ad0c157abdfcd43d1d4ae5216b08c
LeDfee <yermakovdo@gmail.com>
2023-10-09 21:39:44 +0200

    Update Engine.java
    second commit

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Engine {
+public class Engine {
+    private int hpower;
+    private String manuf;
+
+    public Engine(int hpower, String manuf) {
+        this.hpower = hpower;
+        this.manuf = manuf;
+    }
+
+    public int getHpower() {
+        return hpower;
+    }
+
+    public void setHpower(int hpower) {
+        this.hpower = hpower;
+    }
+
+    public String getManuf() {
+        return manuf;
+    }
+
+    public void setManuf(String manuf) {
+        this.manuf = manuf;
+    }
 }
