87fc360f6b3c47f44498acaf4f07731b3ce3a394
LeDfee <yermakovdo@gmail.com>
2023-10-09 21:40:52 +0200

    Update SportCar.java
    second commit

--- src/main/java/SportCar.java
+++ src/main/java/SportCar.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class SportCar {
+public class SportCar extends Car {
+    private int maxSpeed;
+
+    public SportCar(String make, String carClass, double weight, Driver driver, Engine engine, int maxSpeed) {
+        super(make, carClass, weight, driver, engine);
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
 }
